f83a330259be2785d8a629218aace5b35a2b250b
Shawn Arnold <Shawn_Arnold@bose.com>
2023-09-06 11:22:18 -0400

    Add use of the newly created get_ntpqname() method.

--- scan_for_devices.py
+++ scan_for_devices.py
@@ -1,12 +1,5 @@
 #!/usr/bin/python3
 
-G_COMMAND = "ip"
-G_OPTION = "neigh"
-G_MAC_UNIVERSAL = "00:12:34:56:78:9a"
-G_MAC_EEPROM = "00:50:2e:"
-# G_MAC_MATCH = "^(\d+\.\d+\.\d+\.\d+)\s+dev\s+enp7s\d+\s+lladdr\s+(\w\w:\w\w:\w\w:\w\w:\w\w:\w\w)\s+"
-G_MAC_MATCH = "^(\d+\.\d+\.\d+\.\d+)\s+dev\s+eth\d+\s+lladdr\s+(\w\w:\w\w:\w\w:\w\w:\w\w:\w\w)\s+"
-
 import sys
 import device_lists as svlst
 
@@ -25,11 +18,6 @@ if __name__ == '__main__' :
     # for item in dv_list:
     #     print(item)
 
-    np_list = svlst.get_list_ntpq(dv_list)
-    print("\nNetPREQ List:")
-    for item in np_list:
-        print(item)
-
     pq_list = svlst.get_list_pcpq(dv_list)
     svlst.get_hostname(pq_list)
     print("\nPC-PREQ List:")
@@ -40,6 +28,12 @@ if __name__ == '__main__' :
     svlst.get_hostname(lt_list)
     print("\nLaptop List:")
     for item in lt_list:
+        print(item)
+
+    np_list = svlst.get_list_ntpq(dv_list)
+    svlst.get_ntpqname(np_list)
+    print("\nNetPREQ List:")
+    for item in np_list:
         print(item)
 
     print("Python Script: END")
